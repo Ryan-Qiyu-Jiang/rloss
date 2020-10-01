22f1765712702713aca61a4f79b4c82cef7cf31c
Ryan J <ryan.qiyu.jiang@gmail.com>
2020-10-01 14:20:12 -0400

    f

--- pytorch/pytorch-deeplab_v3_plus/trainer_pl.py
+++ pytorch/pytorch-deeplab_v3_plus/trainer_pl.py
@@ -406,7 +406,13 @@ class Mutiscale_Seg_Model(SegModel):
 
         if do_log:
             global_step = i + num_img_tr * epoch
-            output = scaled_outputs[1.0]
+            output_scale = 1.0
+            if self.hparams.rloss_weights[1.0] == 0:
+                output_scale = 0.5
+                if self.hparams.rloss_weights[0.5] == 0:
+                    output_scale = 0.25
+                
+            output = scaled_outputs[output_scale]
             probs = nn.Softmax(dim=1)(output)
             img_entropy = torch.sum(-probs*torch.log(probs+1e-9), dim=1).detach().cpu().numpy()
             color_imgs = []
